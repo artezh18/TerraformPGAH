fd20ac731679500a46702b8f2f04d37e947a26f1
artezh18 <artez18@hotmail.com>
2025-08-09 11:37:38 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+    description = "AKIAZ4LDDSP4YHIHDHVL"
+}
+variable "secret_key" {
+    description = "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"
+}
+variable "region" {
+    description = "AWS region"
+}			
